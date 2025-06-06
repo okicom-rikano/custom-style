48d58cbed3dc39ab9d6194bb481f2a713fb66119
okicom-rikano <82216341+okicom-rikano@users.noreply.github.com>
2025-06-06 17:22:53 +0900

    fix

--- custom.js
+++ custom.js
@@ -1,10 +1,13 @@
-// DOM読み込み完了後に実行するコード
-window.addEventListener('DOMContentLoaded', () => {
-  // 要素の順番を変更する JavaScript コード
-  const filterButton = document.querySelector('button.kb-icon.kb-icon-filter');
-  const filterContainer = document.querySelector('.kb-echo-header-filter');
-  
-  if (filterButton && filterContainer) {
-    filterContainer.appendChild(filterButton);
-  }
-});
+(() => {
+    "use strict";
+    kb.event.on('kb.view.load.complete', (event) => {
+        console.log(event);
+        const filterButton = document.querySelector('button.kb-icon.kb-icon-filter');
+        const filterContainer = document.querySelector('.kb-echo-header-filter');
+        
+        if (filterButton && filterContainer) {
+            filterContainer.appendChild(filterButton);
+        }
+        return event;
+    });
+})();
